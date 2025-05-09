3c86c05fe61a2667ee537aba0f2d6d318ef3edbb
Philosamphy <samanthationg26@gmail.com>
2025-05-09 18:09:18 +0800

    Add files via upload

---
+++ common/tab_widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TabWidget extends StatelessWidget {
+  const TabWidget({super.key, required this.text});
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
